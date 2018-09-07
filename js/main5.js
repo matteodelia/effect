8a8f176c6fa6f791e5ddf7cedfc0343f34e842fc
matteodelia <42676836+matteodelia@users.noreply.github.com>
2018-09-07 18:53:47 +0200

    Update main5.js

--- js/main5.js
+++ js/main5.js
@@ -83,14 +83,14 @@
         // Fit renderer to the screen
         if ( options.fullScreen === true ) {
           renderer.view.style.objectFit = 'cover';
-          renderer.view.style.width     = '100%';
-          renderer.view.style.height    = '100%';
+          renderer.view.style.width     = '50%';
+          renderer.view.style.height    = '50%';
           renderer.view.style.top       = '50%';
           renderer.view.style.left      = '50%';
           renderer.view.style.webkitTransform = 'translate( -50%, -50% ) scale(1.2)';
           renderer.view.style.transform = 'translate( -50%, -50% ) scale(1.2)';           
         } else {
-          renderer.view.style.maxWidth  = '100%';
+          renderer.view.style.maxWidth  = '50%';
           renderer.view.style.top       = '50%';
           renderer.view.style.left      = '50%';
           renderer.view.style.webkitTransform = 'translate( -50%, -50% )';
