53e21ec1cbd52e49225c47ad413a0cf37c3bf16a
matteodelia <42676836+matteodelia@users.noreply.github.com>
2018-09-09 19:36:20 +0200

    Update main5.js

--- js/main5.js
+++ js/main5.js
@@ -13,8 +13,6 @@
       //  OPTIONS
       /// ---------------------------      
       options                     = options || {};
-      options.stageWidth          = options.hasOwnProperty('stageWidth') ? options.stageWidth : 1760;
-      options.stageHeight         = options.hasOwnProperty('stageHeight') ? options.stageHeight : 1176;
       options.pixiSprites         = options.hasOwnProperty('sprites') ? options.sprites : [];
       options.centerSprites       = options.hasOwnProperty('centerSprites') ? options.centerSprites : false;
       options.texts               = options.hasOwnProperty('texts') ? options.texts : [];
